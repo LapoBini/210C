255243ed02820f8ff051113202016fdffc11c41e
LapoBini <lapo.bini96@gmail.com>
2024-05-23 00:02:18 -0700

    .

--- HW2/SVAR.jl
+++ HW2/SVAR.jl
@@ -111,7 +111,7 @@ dic = Dict("R" => R, "u" => u, "P" => P)
 ind_dir  = readdir("./HW2");
 "results" in ind_dir ? nothing : mkdir("./HW2/results");
 
-
+# Plot series
 for i in 1:length(sav)
     ref_months = dic[key[i]].date[1]:Year(5):dic[key[i]].date[end] |> collect .|> DateTime;
     ticks      = DateTime.(unique(year.(Date.(dic[key[i]].date))))[1:5:end] .|> lastdayofmonth;
@@ -272,10 +272,11 @@ end
 # Take the quarterly average
 R = get_data(f, "FEDFUNDS", frequency = "q", aggregation_method = "avg").data[:,end-1:end];
 u = get_data(f, "UNRATE", frequency = "q", aggregation_method = "avg").data[:,end-1:end];
+P = get_data(f, "GDPDEF", frequency = "q").data[:,end-1:end];
 
 # Select the insample dates
-P   = DataFrame([P[5:end,1] (log.(P[5:end,2]) - log.(P[1:end-4,2]))*100], [:date, :value]);
-dic = Dict("R" => R, "u" => u, "P" => P)
+p   = DataFrame([P[5:end,1] (log.(P[5:end,2]) - log.(P[1:end-4,2]))*100], [:date, :value]);
+dic = Dict("R" => R, "u" => u, "P" => p)
 
 for i in 1:length(key)
     aux = findall((dic[key[i]].date .>= Date("01/01/1960","dd/mm/yyyy")) .& 
@@ -301,7 +302,7 @@ var   = ["GDP Deflator"; "Unemployment Rate"; "Fed Funds Rate"];
 shock = ["Inflation Shock", "Unemployment Shock", "Monetary Policy Shock"];
 sav   = ["Chol_inf"; "Chol_un"; "Chol_mon"];
 
-
+# Plot
 for k in 1:size(y,2)
 
     # Prepare Grid
@@ -334,7 +335,6 @@ end
 # Plot Monetary Policy shock using linear relationship εₜ = B₀ uₜ
 ε = (B₀ * U)[3,:];
 
-
 # Compute dates (we lose a year due to )
 ref_quarters = collect(Date("01/01/1960","dd/mm/yyyy"):Quarter(1):Date("01/10/2007", "dd/mm/yyyy"))[5:end];
 ticks        = DateTime.(unique(year.(ref_quarters)))[1:5:end] .|> lastdayofmonth;
@@ -343,7 +343,7 @@ start_sample = "01/01/1961"; end_sample   = "31/12/2007";
 rec          = get_recessions(start_sample, end_sample, series = "USRECM");
 
 # Plot Historical decomposition monerary policy shock 
-plot(ref_quarters .|> DateTime, ε, color = "purple", lw = 2.5, label = "",
+plot(ref_quarters .|> DateTime, ε, color = "deepskyblue", lw = 2.5, label = "",
      size =(800,300), xticks = (ticks,tck_n))
 for sp in rec
     int = Dates.lastdayofmonth(sp[1].-Month(1)) |> DateTime;
@@ -352,34 +352,42 @@ for sp in rec
            alpha = 0.2);
 end
 adj =  Dates.lastdayofmonth(rec[1][1]) |> DateTime;
-vline!([adj], label = "", alpha = 0.0)
+hline!([0], color = "black", lw = .8, label = "")
+vline!([DateTime("01/10/2001", "dd/mm/yyyy")], color = "orange", lw = 1, label = nothing)
+annotate!(DateTime("01/10/2003", "dd/mm/yyyy"), 3.5, Plots.text("Q4/01", "orange", 12))
+vline!([DateTime("01/07/2001", "dd/mm/yyyy")], color = "purple", lw = 1, label = nothing)
+annotate!(DateTime("01/03/1999", "dd/mm/yyyy"), 3.5, Plots.text("Q3/01", "purple", 12))
 plot!(ytickfontsize  = 10, xtickfontsize  = 10, title = "Monetary Policy Shock",
       titlefontsize = 17, yguidefontsize = 13, legendfontsize = 11, 
       boxfontsize = 15, framestyle = :box, left_margin = 4Plots.mm, 
       right_margin = 4Plots.mm, bottom_margin = 2Plots.mm, 
       top_margin = 4Plots.mm, legend = :topleft) 
-
-
+savefig("./HW2/results/reconstructed_mon_shock.pdf")
 
 
 # ------------------------------------------------------------------------------
-# 5 - Functions IRF with Romer and Romer 
+# 5 - Functions IRF with Romer and Romer Shock 
 # ------------------------------------------------------------------------------
 function IRF_RR(
     y,  # T x K
     p,  # lag order of the var
+    s,  # lag order for the shock 
     H,  # horizon IRF
     Z   # exogenous shock 
     )
 
     # --------------------------------------------------------------------------
-    # IRF_CH Impulse response function through cholesky decomposition.
     # Author: Lapo Bini, lbini@ucsd.edu
+    #
+    # IRF_CH Impulse response as in Romer & Romer 2004. Procedure: 
+    # (a) given the model: Yₜ = ∑ₚ βₚ Yₜ₋ₚ + ∑ₛ γₛ Zₜ₋ₛ
+    # (b) estimate Uₜ = Yₜ - ∑ₚ βₚ Yₜ₋ₚ
+    # (c) estimate γₛ regression Uₜ over Zₜ
+    # (d) recursion to compute IRF 
     # --------------------------------------------------------------------------
 
     T, K = size(y);
-    Y    = y[p+1:T,:]';  # 1st column = 1st obs for each K variables
-                         # Y size = K x (T-(p+1))
+    Y    = y[p+1:T,:]'; 
     _, t = size(Y);
     X    = ones(1,T-p);
     
@@ -391,8 +399,51 @@ function IRF_RR(
     # OLS estimation
     B  = (Y*X')/(X*X');      # size = k x (1+(kxp)) 
     U  = Y-B*X;              # Matrix of residuals, size = K x (1+(kxp)) 
-    S  = (U*U')/(t);         # Variance/Covariance Matrix while U'*U RSS
-    B₀ = cholesky(S).L;
+
+    # Get coefficient γ
+    z = Z[p:end];
+    u = U[:,s:end]
+
+    S = zeros(s, size(u,2)) 
+    for j in 1:s
+        S[j,:] = z[s+1-j:end-j]
+    end
+
+    # OLS and companion matrix
+    Γ = (u*S')/(S*S');
+    A = [B[:,2:end]; Matrix(I, K*(p-1), K*(p-1)) zeros(K*(p-1), K)];  
+
+    # Compute Recursion 
+    ΔY = zeros(s, K)
+    ỹ  = zeros(2s, K)
+
+    for i in 1:s
+        
+        ΔY[i,:] = (Γ[:,i] + B[:,2:end]*vec(ΔY[i+7:-1:i,:]'))'
+
+
+
+    yᵢᵣ = zeros(p+Hᵢ, k);
+    ηᵢᵣ = zeros(p+Hᵢ, 1);
+    ηᵢᵣ[p+1] = 1; # shock to the policy measure
+
+    for tt = p+1:Hᵢ+p
+          xᵢᵣ = Array{Float64,2}(undef, p, k)
+          for ji = 1:p
+                xᵢᵣ[ji,:] = yᵢᵣ[tt-ji,:]
+          end
+           xᵢᵣ       = [reshape((xᵢᵣ)', 1, k*p) 0]
+           yᵢᵣ[tt,:] = xᵢᵣ * B + ηᵢᵣ[tt] .* ϕₖ;
+    end
+
+    for r in 1:s
+        Γ[:,s] + 
+        
+
+
+
+
+    
     
     # Create companion matrix and selection matrix
     A   = [B[:,2:end]; Matrix(I, K*(p-1), K*(p-1)) zeros(K*(p-1), K)];  
@@ -492,3 +543,11 @@ function WILD_RR(
 
 end
 
+
+# ------------------------------------------------------------------------------
+# 6 - Produce Result Romer & Romer 
+# ------------------------------------------------------------------------------
+data = DataFrame(XLSX.readtable("./HW2/shock.xlsx", "Sheet2", header = false)) |> Array{Any,2};
+Z    = [zeros(36); data[2:end,end]] # using romer full 
+p    = 8;
+s    = 12;
