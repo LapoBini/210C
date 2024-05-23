b049f74c1055b15dc3f6a9053a821d7ec041b230
LapoBini <lapo.bini96@gmail.com>
2024-05-22 22:22:36 -0700

    mon_pol

--- HW2/SVAR.jl
+++ HW2/SVAR.jl
@@ -2,7 +2,7 @@
 # SVAR
 # ------------------------------------------------------------------------------
 # Author: Lapo Bini, lbini@ucsd.edu
-# Problem Set 2 - Prof. Johannes Wieland - 22/15/2024
+# Problem Set 2 - Prof. Johannes Wieland - 05/22/2024
 # ------------------------------------------------------------------------------
 
 
@@ -266,7 +266,6 @@ function WILD_CH(
 end
 
 
-
 # ------------------------------------------------------------------------------
 # 4 - Estimate IRF by Cholesky 
 # ------------------------------------------------------------------------------
@@ -287,7 +286,7 @@ end
 # Specification VAR
 y    = [dic["P"].value dic["u"].value dic["R"].value] |> Array{Float64,2};
 p    = 4;
-H    = 20;
+H    = 32;
 nrep = 1000;
 
 # Estimation SVAR 
@@ -303,21 +302,193 @@ shock = ["Inflation Shock", "Unemployment Shock", "Monetary Policy Shock"];
 sav   = ["Chol_inf"; "Chol_un"; "Chol_mon"];
 
 
-j = 3; k = 3;
-plot(size = (700,600), ytickfontsize  = 15, xtickfontsize  = 15,
-     xguidefontsize = 15, legendfontsize = 13, boxfontsize = 15,
-     framestyle = :box, yguidefontsize = 15, titlefontsize = 20)
+for k in 1:size(y,2)
 
-# Plot bootstrap confidence interval 
-for a in 1:size(Low, 4)
-    plot!(x_ax, [IRF[1,j,k]; Low[2:end,j,k,a]], fillrange = [IRF[1,j,k]; Upp[2:end,j,k,a]],
-          lw = 1, alpha = c[a], color = "deepskyblue1", xticks = ticks,
-          label = "")
+    # Prepare Grid
+    plot(layout = grid(3,1), size = (1200,1100), ytickfontsize  = 15, xtickfontsize  = 15,
+        xguidefontsize = 15, legendfontsize = 13, boxfontsize = 15,
+        framestyle = :box, yguidefontsize = 15, titlefontsize = 20,
+        left_margin = 1Plots.mm, right_margin = 3Plots.mm,
+        bottom_margin = 1Plots.mm, top_margin = 3Plots.mm)
+    plot!(xlabel = "Quarters", ylabel = "", xlims = (0,H), subplot = j)
+    plot!(title = shock[k], subplot = 1)
+
+    for j in 1:size(y,2)
+
+        # Plot bootstrap confidence interval 
+        for a in 1:size(Low, 4)
+            plot!(x_ax, [IRF[1,j,k]; Low[2:end,j,k,a]]./IRF[1,k,k], fillrange = [IRF[1,j,k]; Upp[2:end,j,k,a]]./IRF[1,k,k],
+                lw = 1, alpha = c[a], color = "deepskyblue1", xticks = ticks,
+                label = "", subplot = j)
+        end
+
+        # Plot IRF and steady state zero line
+        Plots.plot!(x_ax, IRF[:,j,k]./IRF[1,k,k], lw = 3, color = "black", xticks = ticks,
+                    label = var[j], subplot = j)
+        hline!([0], color = "black", lw = 1, label = nothing, subplot = j, xlims = (0,H))
+    end
+
+    savefig("./HW2/results/"*sav[k]*".pdf")
 end
-Plots.plot!(x_ax, IRF[:,j,k], lw = 3, color = "black", xticks = ticks,
-            label = var[j])
-hline!([0], color = "black", lw = 1, label = nothing)
-Plots.plot!(xlabel = "Quarters", ylabel = "", title = shock[k],
-                    left_margin = 1Plots.mm, right_margin = 3Plots.mm,
-                    bottom_margin = 1Plots.mm, top_margin = 3Plots.mm,
-                    xlims = (0,H))
+
+# Plot Monetary Policy shock using linear relationship εₜ = B₀ uₜ
+ε = (B₀ * U)[3,:];
+
+
+# Compute dates (we lose a year due to )
+ref_quarters = collect(Date("01/01/1960","dd/mm/yyyy"):Quarter(1):Date("01/10/2007", "dd/mm/yyyy"))[5:end];
+ticks        = DateTime.(unique(year.(ref_quarters)))[1:5:end] .|> lastdayofmonth;
+tck_n        = Dates.format.(Date.(ticks), "Y");
+start_sample = "01/01/1961"; end_sample   = "31/12/2007"; 
+rec          = get_recessions(start_sample, end_sample, series = "USRECM");
+
+# Plot Historical decomposition monerary policy shock 
+plot(ref_quarters .|> DateTime, ε, color = "purple", lw = 2.5, label = "",
+     size =(800,300), xticks = (ticks,tck_n))
+for sp in rec
+    int = Dates.lastdayofmonth(sp[1].-Month(1)) |> DateTime;
+    fnl = Dates.lastdayofmonth(sp[2].-Month(1)) |> DateTime;
+    vspan!([int, fnl], label = "", color = "grey0",
+           alpha = 0.2);
+end
+adj =  Dates.lastdayofmonth(rec[1][1]) |> DateTime;
+vline!([adj], label = "", alpha = 0.0)
+plot!(ytickfontsize  = 10, xtickfontsize  = 10, title = "Monetary Policy Shock",
+      titlefontsize = 17, yguidefontsize = 13, legendfontsize = 11, 
+      boxfontsize = 15, framestyle = :box, left_margin = 4Plots.mm, 
+      right_margin = 4Plots.mm, bottom_margin = 2Plots.mm, 
+      top_margin = 4Plots.mm, legend = :topleft) 
+
+
+
+
+# ------------------------------------------------------------------------------
+# 5 - Functions IRF with Romer and Romer 
+# ------------------------------------------------------------------------------
+function IRF_RR(
+    y,  # T x K
+    p,  # lag order of the var
+    H,  # horizon IRF
+    Z   # exogenous shock 
+    )
+
+    # --------------------------------------------------------------------------
+    # IRF_CH Impulse response function through cholesky decomposition.
+    # Author: Lapo Bini, lbini@ucsd.edu
+    # --------------------------------------------------------------------------
+
+    T, K = size(y);
+    Y    = y[p+1:T,:]';  # 1st column = 1st obs for each K variables
+                         # Y size = K x (T-(p+1))
+    _, t = size(Y);
+    X    = ones(1,T-p);
+    
+    for j=1:p
+        x = y[p+1-j:T-j,:]';  # Y_t-p such that 1st column = 1st obs x each variab
+        X = [X; x];           # final size: X = (1+(Kxp)) x (T-(p+1)) 
+    end
+    
+    # OLS estimation
+    B  = (Y*X')/(X*X');      # size = k x (1+(kxp)) 
+    U  = Y-B*X;              # Matrix of residuals, size = K x (1+(kxp)) 
+    S  = (U*U')/(t);         # Variance/Covariance Matrix while U'*U RSS
+    B₀ = cholesky(S).L;
+    
+    # Create companion matrix and selection matrix
+    A   = [B[:,2:end]; Matrix(I, K*(p-1), K*(p-1)) zeros(K*(p-1), K)];  
+    J   = [Matrix(I, K, K) zeros(K, K*(p-1))];
+
+    # Allocation results
+    IRF = zeros(H+1, K, K);
+
+    for k in 1:K 
+        IRF[1,:,k] = B₀[:,k]';
+
+        # Compute IRF
+        for h=1:H
+            M   = J*A^h*J';
+            MM  = B₀*M;
+            IRF[h+1,:,k] = MM[:,k]';
+        end
+    end
+    
+    return IRF, B, U, B₀
+
+end
+
+function WILD_RR(
+    y,   # data
+    B,   # OLS coeff
+    u,   # residual
+    p,   # lag order
+    H,   # forecast horizon
+    nrep # bootstrap repetitions
+    )
+
+    # --------------------------------------------------------------------------
+    # WILD_CH Compute the confidence interval using wild bootstrap
+    # It computes the confidence interval accounting for the
+    # heteroskedasticity in the error term.
+    #
+    # How I compute the quantiles: I am going to apply the quantile function 
+    # to slices of IRFS along the fourth dimension (bootstrap repetitions).
+    # The function u -> quantile(u, a) computes the specified quantiles for
+    # each slice. The dims=(4,) argument specifies that the function should 
+    # be applied along the fourth dimension. The last dimension of the output
+    # matrix will give the quantiles 
+    #
+    # Author: Lapo Bini, lbini@ucsd.edu
+    # --------------------------------------------------------------------------
+    
+    # Set up variables for iteration 
+    counter = 1;
+    a       = [.05, 0.1, 0.36];
+    T, K    = size(y);
+    
+    # Matrix which will save the results
+    IRFS = zeros(H+1, K, K, nrep-1);      # nrep-simulations for IRF
+    Low  = zeros(H+1, length(a), K, K);   # lower quantiles for each yₜ
+    Upp  = zeros(H+1, length(a), K, K);   # upper quantiles for each yₜ
+    
+    # Bootstrap preliminaries
+    data = [-1 1];          # for the random draw 
+    U    = u';              # Residuals in TxK dimension
+    yᵇ   = zeros(T, K);     # for saving the simulated Yₜ
+    A    = B[:,2:end];      # matrix of coefficients for yₜ₋₁, ⋯ , yₜ₋ₚ
+    A₀   = B[:,1];          # Intercept
+    
+    # Bootstrap loop 
+    while counter < nrep
+
+        # Random draw to correct for heteroskedasticity w ~ [-1,1]
+        w  = rand(data, T);   
+        uᵇ = U.*w[p+1:end];  
+        
+        # the first p-observations are fixed 
+        for j=1:K
+            for i=1:p
+                yᵇ[i, j] = y[i, j];
+            end
+        end
+        
+        # Generate bootstrap sample Yₜᵇ for t = p+1,...,T.
+        for j = (p+1):T
+            xᵇ      = yᵇ[j-1:-1:j-p, :]';                      
+            yᵇ[j,:] = A₀ + A*vec(xᵇ) + uᵇ[j-p,:] 
+        end
+        
+        # Compute IRF on bootstrap sample and save results
+        IRFS[:,:,:,counter] = IRF_CH(yᵇ, p, H)[1];
+
+        # Update counter 
+        counter += 1; 
+    end
+    
+    # Compute quantiles 
+    Low = mapslices(u->quantile(u, a./2), IRFS, dims=(4,))
+    Upp = mapslices(u->quantile(u, 1.0.-a./2), IRFS, dims=(4,))
+
+    return Low, Upp
+
+end
+
