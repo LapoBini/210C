d14d9796fb52986c1fd5e05d690f35c26d860add
LapoBini <lapo.bini96@gmail.com>
2024-05-22 20:50:30 -0700

    Cholesky and wild bootstrap

--- HW2/SVAR.jl
+++ HW2/SVAR.jl
@@ -137,5 +137,157 @@ end
 
 
 # ------------------------------------------------------------------------------
-# 2 - Plot Data
+# 3 - Choleski Function
 # ------------------------------------------------------------------------------
+function IRF_CH(
+    y,  # T x K
+    p,  # lag order of the var
+    H,  # horizon IRF
+    )
+
+    # --------------------------------------------------------------------------
+    # IRF_CH Impulse response function through cholesky decomposition.
+    # Author: Lapo Bini, lbini@ucsd.edu
+    # --------------------------------------------------------------------------
+
+    T, K = size(y);
+    Y    = y[p+1:T,:]';  # 1st column = 1st obs for each K variables
+                         # Y size = K x (T-(p+1))
+    _, t = size(Y);
+    X    = ones(1,T-p);
+    
+    for j=1:p
+        x = y[p+1-j:T-j,:]';  # Y_t-p such that 1st column = 1st obs x each variab
+        X = [X; x];           # final size: X = (1+(Kxp)) x (T-(p+1)) 
+    end
+    
+    # OLS estimation
+    B  = (Y*X')/(X*X');      # size = k x (1+(kxp)) 
+    U  = Y-B*X;              # Matrix of residuals, size = K x (1+(kxp)) 
+    S  = (U*U')/(t);         # Variance/Covariance Matrix while U'*U RSS
+    B₀ = cholesky(S).L;
+    
+    # Create companion matrix and selection matrix
+    A   = [B[:,2:end]; Matrix(I, K*(p-1), K*(p-1)) zeros(K*(p-1), K)];  
+    J   = [Matrix(I, K, K) zeros(K, K*(p-1))];
+
+    # Allocation results
+    IRF = zeros(H+1, K, K);
+
+    for k in 1:K 
+        IRF[1,:,k] = B₀[:,k]';
+
+        # Compute IRF
+        for h=1:H
+            M   = J*A^h*J';
+            MM  = B₀*M;
+            IRF[h+1,:,k] = MM[:,k]';
+        end
+    end
+    
+    return IRF, B, U, B₀
+
+end
+
+function WILD_CH(
+    y,   # data
+    B,   # OLS coeff
+    u,   # residual
+    p,   # lag order
+    H,   # forecast horizon
+    nrep # bootstrap repetitions
+    )
+
+    # --------------------------------------------------------------------------
+    # WILD_CH Compute the confidence interval using wild bootstrap
+    # It computes the confidence interval accounting for the
+    # heteroskedasticity in the error term.
+
+    # How I compute the quantiles: I am going to apply the quantile function 
+    # to slices of IRFS along the fourth dimension (bootstrap repetitions).
+    # The function u -> quantile(u, a) computes the specified quantiles for
+    # each slice. The dims=(4,) argument specifies that the function should 
+    # be applied along the fourth dimension. The last dimension of the output
+    # matrix will give the quantiles 
+    #
+    # Author: Lapo Bini, lbini@ucsd.edu
+    # --------------------------------------------------------------------------
+    
+    # Set up variables for iteration 
+    counter = 1;
+    a       = [.05, .10, .15];
+    T, K    = size(y);
+    
+    # Matrix which will save the results
+    IRFS = zeros(H+1, K, K, nrep-1);      # nrep-simulations for IRF
+    Low  = zeros(H+1, length(a), K, K);   # lower quantiles for each yₜ
+    Upp  = zeros(H+1, length(a), K, K);   # upper quantiles for each yₜ
+    
+    # Bootstrap preliminaries
+    data = [-1 1];          # for the random draw 
+    U    = u';              # Residuals in TxK dimension
+    yᵇ   = zeros(T, K);     # for saving the simulated Yₜ
+    A    = B[:,2:end];      # matrix of coefficients for yₜ₋₁, ⋯ , yₜ₋ₚ
+    A₀   = B[:,1];          # Intercept
+    
+    # Bootstrap loop 
+    while counter < nrep
+
+        # Random draw to correct for heteroskedasticity w ~ [-1,1]
+        w  = rand(data, T);   
+        uᵇ = U.*w[p+1:end];  
+        
+        # the first p-observations are fixed 
+        for j=1:K
+            for i=1:p
+                yᵇ[i, j] = y[i, j];
+            end
+        end
+        
+        # Generate bootstrap sample Yₜᵇ for t = p+1,...,T.
+        for j = (p+1):T
+            xᵇ      = yᵇ[j-1:-1:j-p, :]';                      
+            yᵇ[j,:] = A₀ + A*vec(xᵇ) + uᵇ[j-p,:] 
+        end
+        
+        # Compute IRF on bootstrap sample and save results
+        IRFS[:,:,:,counter] = IRF_CH(yᵇ, p, H)[1];
+
+        # Update counter 
+        counter += 1; 
+    end
+    
+    # Compute quantiles 
+    Low = mapslices(u->quantile(u, a./2), IRFS, dims=(4,))
+    Upp = mapslices(u->quantile(u, 1.0.-a./2), IRFS, dims=(4,))
+
+    return Low, Upp
+
+end
+
+
+
+# ------------------------------------------------------------------------------
+# 4 - Estimate IRF by Cholesky 
+# ------------------------------------------------------------------------------
+# Take the quarterly average
+R = get_data(f, "FEDFUNDS", frequency = "q", aggregation_method = "avg").data[:,end-1:end];
+u = get_data(f, "UNRATE", frequency = "q", aggregation_method = "avg").data[:,end-1:end];
+
+# Select the insample dates
+dic = Dict("R" => R, "u" => u, "P" => P)
+for i in 1:length(key)
+    # Pick the correct date 
+    aux = findall((dic[key[i]].date .>= Date("01/01/1960","dd/mm/yyyy")) .& 
+                  (dic[key[i]].date .<= Date("01/10/2007", "dd/mm/yyyy")));
+    dic[key[i]] = dic[key[i]][aux,:];
+end
+
+# Specification VAR
+y    = [log.(dic["P"].value) dic["u"].value dic["R"].value];
+p    = 4;
+H    = 50;
+nrep = 5000;
+
+IRF, B, U, B₀ = IRF_CH(y, p, H);
+Low, Upp      = WILD_CH(y, B, U, p, H, nrep);
